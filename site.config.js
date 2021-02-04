296fc53cd67af157f18c110f7509d6add164395d
İbrahim Uzun <info@ibrahimuzun.com>
2021-02-04 20:37:17 +0300

    Development

--- site.config.js
+++ site.config.js
@@ -1,31 +1,31 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '5f9e54dc684343c3bb1becb2ed6f48fe',
 
   // if you want to restrict pages to a single notion workspace (optional)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'İbrahim Uzun',
+  domain: 'ibrahimuzun.com',
+  author: 'İbrahim Uzun',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'İbrahim Uzun',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'ibrahimuzn',
+  github: 'iuzn',
+  linkedin: 'ibuzn',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
   defaultPageIcon:
-    'https://ssfy.io/https%3A%2F%2Fwww.notion.so%2Fimage%2Fhttps%253A%252F%252Fs3-us-west-2.amazonaws.com%252Fsecure.notion-static.com%252F797768e4-f24a-4e65-bd4a-b622ae9671dc%252Fprofile-2020-280w-circle.png%3Ftable%3Dblock%26id%3D78fc5a4b-88d7-4b0e-824e-29407e9f1ec1%26cache%3Dv2',
+    'https://raw.githubusercontent.com/iuzn/assets/master/Developer%20Blog/dev-favicon.png',
   defaultPageCover:
-    'https://ssfy.io/https%3A%2F%2Fwww.notion.so%2Fimage%2Fhttps%253A%252F%252Fs3-us-west-2.amazonaws.com%252Fsecure.notion-static.com%252F9fc5ecae-2b4b-4e73-b0d4-918c829ba69f%252FIMG_0259-opt.jpg%3Ftable%3Dblock%26id%3D78fc5a4b-88d7-4b0e-824e-29407e9f1ec1%26cache%3Dv2',
+    'https://raw.githubusercontent.com/iuzn/assets/master/Developer%20Blog/dev-avatar.png',
   defaultPageCoverPosition: 0.5,
 
   // image CDN host to proxy all image requests through (optional)
